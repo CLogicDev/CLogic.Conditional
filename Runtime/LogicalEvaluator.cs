74d5133294ff02de93d1a0422241dfd736728dcc
Shaun Maurer Masson <shaun@clogic.dev>
2026-04-24 11:39:08 +0400

    feat: Logical evaluator

---
+++ Runtime/LogicalEvaluator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+namespace CLogic.Conditionals
+{
+    public class LogicalEvaluator : ConditionalEvaluator
+    {
+        public enum LogicalOperationType
+        {
+            NOT,
+            AND,
+            OR,
+            XOR,
+            XNOR
+        }
+        
+        public ConditionalEvaluator[] evaluators;
+        public LogicalOperationType logicalOperationType;
+        
+        public override bool Evaluate()
+        {
+            return logicalOperationType switch
+            {
+                LogicalOperationType.NOT => !evaluators[0],
+                LogicalOperationType.AND => evaluators.All(x => x.Evaluate()),
+                LogicalOperationType.OR => evaluators.Any(x => x.Evaluate()),
+                LogicalOperationType.XOR => evaluators.Count(x => x.Evaluate()) % 2 == 1,
+                LogicalOperationType.XNOR => evaluators.Count(x => x.Evaluate()) % 2 == 0,
+                _ => throw new ArgumentOutOfRangeException()
+            };
+        }
+    }
+}
